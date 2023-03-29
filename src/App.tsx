be12e4d9e83e2608cabc630b17cc37866caeaeb8
Ciahaka <wilddiver.by@gmail.com>
2023-03-29 21:58:24 +0300

    add func for add new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
+import {v1} from 'uuid';
 
 export type ChangeFilterType = 'All' | 'Active' | 'Completed'
 
 
 function App() {
   let [tasks, setTasks] = useState([
-    {id: 1, title: 'HTML&CSS', isDone: true},
-    {id: 2, title: 'JS', isDone: true},
-    {id: 3, title: 'ReactJS', isDone: false},
-    {id: 4, title: 'Rest API', isDone: true},
-    {id: 5, title: 'GraphQL', isDone: false},
+    {id: v1(), title: 'HTML&CSS', isDone: true},
+    {id: v1(), title: 'JS', isDone: true},
+    {id: v1(), title: 'ReactJS', isDone: false},
+    {id: v1(), title: 'Rest API', isDone: true},
+    {id: v1(), title: 'GraphQL', isDone: false},
   ])
   let [filter, setFilter] = useState<ChangeFilterType>('All')
 
 
-  const removeTasks = (id: number) => {
+  const removeTasks = (id: string) => {
     tasks = tasks.filter(t => t.id !== id)
     setTasks(tasks)
   }
-  const changeFilter = (value:ChangeFilterType)=> setFilter(value)
+  const changeFilter = (value: ChangeFilterType) => setFilter(value)
+  const addTask = (title:string) => {
+    let newTask = {id: v1(), title, isDone: false}
+    const newTasksArr = [newTask,...tasks]
+    setTasks(newTasksArr)
+  }
 
   let filteredTasks = tasks
-  if (filter ==='Active' ){
-    filteredTasks = tasks.filter((t)=>!t.isDone)
+  if (filter === 'Active') {
+    filteredTasks = tasks.filter((t) => !t.isDone)
   }
-  if (filter ==='Completed' ){
-    filteredTasks = tasks.filter((t)=>t.isDone)
+  if (filter === 'Completed') {
+    filteredTasks = tasks.filter((t) => t.isDone)
   }
 
 
@@ -37,6 +43,7 @@ function App() {
                 tasks={filteredTasks}
                 removeTasks={removeTasks}
                 changeFilter={changeFilter}
+                addTask={addTask}
       />
     </div>
 
