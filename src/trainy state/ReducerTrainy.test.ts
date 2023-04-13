c27b98220b90ed210b3e48a648fc45fea7f13d6f
Ciahaka <wilddiver.by@gmail.com>
2023-04-13 22:23:01 +0300

    add new test for trainy reducer

--- src/trainy state/ReducerTrainy.test.ts
+++ src/trainy state/ReducerTrainy.test.ts
@@ -8,10 +8,16 @@ test('user reducer should increment only age', () => {
   expect(endState.age).toBe(43)
   expect(endState.childrenCount).toBe(1)
 })
-
 test('user reducer should increment only childrenCount', () => {
   const startState = {age: 42, childrenCount: 1, name: 'Iscander'}
   const endState = userReducer(startState, {type: 'INCREMENT-CHILDREN-COUNT'})
   expect(endState.childrenCount).toBe(2)
   expect(endState.age).toBe(42)
+})
+test('user reducer should change name of user', () => {
+  const startState = {age: 42, childrenCount: 1, name: 'Iscander'}
+  const newName = 'Shunya'
+  const endState = userReducer(startState, {type: 'CHANGE-NAME', newName: newName})
+
+  expect(endState.name).toBe(newName)
 })
