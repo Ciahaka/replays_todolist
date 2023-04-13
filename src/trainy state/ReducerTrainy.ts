6f94a097026af5a82403ce0ba6bbb5c4e3339daa
Ciahaka <wilddiver.by@gmail.com>
2023-04-13 22:23:37 +0300

    add new action into trine reducer

--- src/trainy state/ReducerTrainy.ts
+++ src/trainy state/ReducerTrainy.ts
@@ -1,4 +1,3 @@
-
 type StateType = {
   age: number
   childrenCount: number
@@ -9,15 +8,26 @@ type ActionType = {
   [key: string]: any
 }
 
-export const userReducer = (state: StateType, action: ActionType):StateType => {
+export const userReducer = (state: StateType, action: ActionType): StateType => {
   switch (action.type) {
     case 'INCREMENT-AGE':
-      state.age = state.age + 1
-      return state
+      return {
+        ...state,
+        age: state.age + 1
+      }
     case 'INCREMENT-CHILDREN-COUNT':
-      state.childrenCount = state.childrenCount + 1
-      return state
-    default: throw new Error('Error! Action crash me! Error!')
+      return {
+        ...state,
+        childrenCount: state.childrenCount + 1
+      }
+    case 'CHANGE-NAME':
+      let newName = 'Shunya'
+      return {
+        ...state,
+        name: newName
+      }
+    default:
+      throw new Error('Error! Action crash me! Error!')
   }
 
 }
