63a8d4b1b58ef3d233517d46afbe4ebd31e6eed4
Ciahaka <wilddiver.by@gmail.com>
2023-04-18 22:11:43 +0300

    TDD, add unit test lдля проверки функции удаления тудулиста в tasks_reducer.ts

--- src/BLL/tasks reducer/tasks_todo_reducers.test.ts
+++ src/BLL/tasks reducer/tasks_todo_reducers.test.ts
@@ -1,4 +1,9 @@
-import {addTodolistAC, todolistReducer} from '../todolist reducer/todolist_reducer';
+import {
+  addTodolistAC,
+  removeTodolistAC,
+  removeTodolistActionType,
+  todolistReducer
+} from '../todolist reducer/todolist_reducer';
 import {TasksStateType, TodolistType} from '../../component/App';
 import {tasksReducer} from './tasks_reducer';
 
@@ -17,4 +22,29 @@ test('ids should be equals', () => {
 
   expect(idFromTasks).toBe(action.id)
   expect(idFromTodolist).toBe(action.id)
+})
+
+test('property with todolistId should be deleted', () => {
+  const startState: TasksStateType = {
+    'todolistId1': [
+      {id: '1', title: 'CSS', isDone: false},
+      {id: '2', title: 'JS', isDone: true},
+      {id: '3', title: 'React', isDone: false}
+    ],
+    'todolistId2': [
+      {id: '1', title: 'bread', isDone: false},
+      {id: '2', title: 'milk', isDone: true},
+      {id: '3', title: 'tea', isDone: false}
+    ]
+  }
+
+  const action:removeTodolistActionType = removeTodolistAC('todolistId2')
+
+  const endState = tasksReducer(startState, action)
+
+
+  const keys = Object.keys(endState)
+
+  expect(keys.length).toBe(1)
+  expect(endState['todolistId2']).not.toBeDefined()
 })
